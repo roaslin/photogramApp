b98ce3348fe2fa49f580fe0259b0a133af804250
Raul Mordillo Lluva <raul.mordillo@koahealth.com>
2022-11-26 21:43:33 +0100

    Fix bad config in package json

--- photogram_api/src/__tests__/postsrepository.test.js
+++ photogram_api/src/__tests__/postsrepository.test.js
@@ -1,7 +1,8 @@
 describe('PostsRepository should', () => {
   test('return empty posts when user is not following anyone', () => {
     // const result =
+    const caca = 'daad';
 
-    expect(true).toEquals(false);
+    expect('true').toEquals(caca);
   });
 });
