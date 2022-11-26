b3f0bf2b9fabe9540b080c88af726cd91c7f9111
Raul Mordillo Lluva <raul.mordillo@koahealth.com>
2022-11-26 22:29:11 +0100

    (integration-tests): setup docker db for integration tests, Add one test WIP

--- photogram_api/src/__tests__/postsrepository.test.js
+++ photogram_api/src/__tests__/postsrepository.test.js
@@ -1,8 +1,41 @@
-describe('PostsRepository should', () => {
-  test('return empty posts when user is not following anyone', () => {
-    // const result =
-    const caca = 'daad';
+const { Pool } = require('pg');
 
-    expect('true').toEquals(caca);
+describe('PostsRepository should', () => {
+  let pool;
+  let client;
+  beforeAll(async () => {
+    pool = new Pool({
+      host: 'localhost',
+      database: 'postgres',
+      user: 'postgres',
+      password: 'example',
+      port: 5433,
+    });
+
+    client = {
+      async query(text, params) {
+        try {
+          const res = await pool.query(text, params);
+          return res;
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    };
+
+    await client.query('DELETE FROM hashtags WHERE true');
+  });
+
+  afterAll(async () => {
+    await client.query('DELETE FROM hashtags WHERE true');
+    await pool.end();
+  });
+
+  test('return empty posts when user is not following anyone', async () => {
+    await client.query('INSERT INTO hashtags (title) VALUES ($1)', ['tutti']);
+
+    const result = await client.query('SELECT * FROM hashtags');
+
+    expect(result.rows[0].title).toEqual('tutti');
   });
 });
