03326a08e3c28a7c37958b3f5d330fb73991e074
Raul Mordillo Lluva <raul.mordillo@koahealth.com>
2022-11-26 21:19:05 +0100

    Update

---
+++ photogram_api/src/__tests__/postsrepository.spec.js
@@ -0,0 +1,7 @@
+describe('PostsRepository should', () => {
+  test('return empty posts when user is not following anyone', () => {
+    // const result =
+
+    expect(true).toEquals(false);
+  });
+});
