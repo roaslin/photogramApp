d8e63f2b64da5c2cdcbc64330a2cde1bef59ebd0
Raul Mordillo Lluva <raul.mordillo@koahealth.com>
2022-11-27 20:36:30 +0100

    test(postrepo): Return posts from users a user is following

--- photogram_api/src/__tests__/postsrepository.test.js
+++ photogram_api/src/__tests__/postsrepository.test.js
@@ -51,11 +51,43 @@ describe('PostsRepository should', () => {
     expect(result.rows.length).toEqual(0);
   });
 
-  // test('return empty posts when user is not following anyone', async () => {
-  //   await client.query('INSERT INTO hashtags (title) VALUES ($1)', ['tutti']);
+  test("return friend's posts when user is following friends", async () => {
+    await client.query(
+      'INSERT INTO users (id, username,email) VALUES ($1,$2,$3)',
+      [1, 'ihaveOnefriend', 'ihaveonefriend@test.com']
+    );
 
-  //   const result = await client.query('SELECT * FROM hashtags');
+    await client.query(
+      'INSERT INTO users (id,username,email) VALUES ($1,$2,$3)',
+      [2, 'iamafriend', 'iamafriend@test.com']
+    );
 
-  //   expect(result.rows[0].title).toEqual('tutti');
-  // });
+    await client.query(
+      'INSERT INTO followers (leader_id,follower_id) VALUES ($1,$2)',
+      [2, 1]
+    );
+
+    await client.query(
+      'INSERT INTO posts (url, caption, lat, lng, user_id) VALUES ($1,$2,$3,$4,$5)',
+      [
+        'http://iamafriend.com/picture',
+        'Awesome pic of my nice furniture',
+        43.3,
+        45.6,
+        2,
+      ]
+    );
+
+    const result = await client.query(
+      `SELECT p.*
+        FROM users u
+      INNER JOIN followers f ON u.id = f.follower_id
+      INNER JOIN posts p ON f.leader_id  = p.user_id  
+      WHERE u.id = 1
+      ORDER BY p.created_at DESC
+      LIMIT 10`
+    );
+
+    expect(result.rows.length).toEqual(1);
+  });
 });
